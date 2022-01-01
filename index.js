00735eb3f583d7412db85435fe4854f602dd6f6f
Abu-Nayeem20 <abu64673@gmail.com>
2022-01-01 21:27:20 +0600

    added

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 const fileUpload = require('express-fileupload');
 
-// const Stripe = require('stripe');
-// const stripe = Stripe(`${process.env.STRIPE_SECRET}`);
+const Stripe = require('stripe');
+const stripe = Stripe(`${process.env.STRIPE_SECRET}`);
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,9 +15,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
-
-// hero_rider
-// HGSoPzqxHVI39Gbs
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f1hps.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -30,6 +27,8 @@ async function run() {
         const database = client.db('heroRider');
         const learnerCollection = database.collection('learners');
         const riderCollection = database.collection('riders');
+        const packageCollection = database.collection('packages');
+        const orderCollection = database.collection('orders');
     
         // POST API FOR LEARNERS
         app.post('/learners', async (req, res) => {
@@ -55,7 +54,8 @@ async function run() {
                 address,
                 phone,
                 type: "learner",
-                status: "active",
+                status: "Active",
+                role: "User",
                 nidImg: nidImageBuffer,
                 profileImg: profileImageBuffer
             }
@@ -99,7 +99,8 @@ async function run() {
                 address,
                 phone,
                 type: "rider",
-                status: "active",
+                status: "Active",
+                role: "User",
                 drivingLicenceImg: drivingLicenceImgBuffer,
                 area,
                 nidImg: nidImageBuffer,
@@ -110,6 +111,21 @@ async function run() {
             }
             const result = await riderCollection.insertOne(rider);
             res.json(result);
+        });
+
+         // GET PACKAGES API 
+         app.get('/packages', async (req, res) => {
+            const cursor = packageCollection.find({});
+            const packages = await cursor.toArray();
+            res.send(packages);
+        });
+
+        // GET API FOR A SINGLE PACKAGE
+        app.get('/packages/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const package = await packageCollection.findOne(query);
+            res.json(package);
         });
         
         // GET RIDER INFO
@@ -131,6 +147,105 @@ async function run() {
             const learner = await cursor.toArray();
             res.json(learner);
         });
+
+        // POST API FOR ORDERS
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const newOrder = {...order, status: "Pending"};
+            const result = await orderCollection.insertOne(newOrder);
+            // console.log("new order added");
+            res.json(result);
+        });
+
+         // GET RIDER USERS API 
+         app.get('/riderUsers', async (req, res) => {
+            const cursor = riderCollection.find({});
+            const riders = await cursor.toArray();
+            res.send(riders);
+        });
+
+         // GET RIDER USERS API 
+         app.get('/learnerUsers', async (req, res) => {
+            const cursor = learnerCollection.find({});
+            const learners = await cursor.toArray();
+            res.send(learners);
+        });
+
+        // UPDATE API FOR USERS
+        app.put('/riderUsers/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedRider = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    status: updatedRider.status
+                }
+            };
+            const result = await riderCollection.updateOne(filter, updatedDoc, options);
+            res.json(result);
+        });
+
+        // UPDATE API FOR USERS
+        app.put('/riderUsers/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedRider = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    role: updatedRider.role
+                }
+            };
+            const result = await riderCollection.updateOne(filter, updatedDoc, options);
+            res.json(result);
+        });
+
+        // UPDATE API FOR USERS
+        app.put('/learnerUsers/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedLearner = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    status: updatedLearner.status
+                }
+            };
+            const result = await learnerCollection.updateOne(filter, updatedDoc, options);
+            res.json(result);
+        });
+
+        // UPDATE API FOR USERS
+        app.put('/learnerUsers/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedLearner = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    role: updatedLearner.role
+                }
+            };
+            const result = await learnerCollection.updateOne(filter, updatedDoc, options);
+            res.json(result);
+        });
+
+        // STRIPE PAYMENT API
+        app.post('/create-payment-intent', async (req, res) => {
+            const paymentInfo = req.body;
+            const amount = paymentInfo.price * 100;
+          
+            const paymentIntent = await stripe.paymentIntents.create({
+              amount: amount,
+              currency: 'usd',
+              payment_method_types: ['card']
+            });
+            // console.log(paymentIntent.client_secret)
+            res.send({
+              clientSecret: paymentIntent.client_secret,
+            });
+          });
     
 
     }
